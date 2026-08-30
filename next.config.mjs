14d2eff21b586a69e474526e6ae61f5c691fe4d4
cerrae <suncquinones@gmail.com>
2026-08-30 18:17:52 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/suncerrae',
-  images: { unoptimized: true },
-};
-
-export default nextConfig;
+ /** @type {import('next').NextConfig} */
+-const nextConfig = {
++const basePath = '/suncerrae';
++
++const nextConfig = {
+   output: 'export',
+-  basePath: '/suncerrae',
++  basePath,
++  env: { NEXT_PUBLIC_BASE_PATH: basePath },
+   images: { unoptimized: true },
+ };
